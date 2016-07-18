35338522e2c4a39fce85cc84bf90f927b08d6aba
David Bradway <dpb6@duke.edu>
2016-07-18 11:10:58 -0400

    fix typo

--- myScript.m
+++ myScript.m
@@ -1,5 +1,5 @@
 % 'a' contains the SLURM_ARRAY_TASK_ID
-% you could let it point do a different directory or input data file
+% you could let it point to a different directory or input data file
 
 b=a+a;
 disp(b)
